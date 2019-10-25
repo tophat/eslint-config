f6642f44cac4645c1cb1886a2227f37b7b89e8c1
Michael Rose <michael.rose@tophatmonocle.com>
2019-10-25 16:49:49 -0400

    Add base config

--- index.js
+++ index.js
@@ -1,37 +1,7 @@
 module.exports = {
+    extends: ['./base', './react', './jest'].map(require.resolve),
     parserOptions: {
         ecmaVersion: 9,
         sourceType: 'module',
-    },
-    env: {
-        node: true,
-        browser: true,
-        es6: true,
-    },
-    extends: ['eslint:recommended', 'prettier'],
-    plugins: ['prettier'],
-    rules: {
-        'prettier/prettier': [
-            'error',
-            {
-                printWidth: 80,
-                tabWidth: 4,
-                semi: false,
-                trailingComma: 'all',
-                singleQuote: true,
-            },
-        ],
-        camelcase: ['error', { properties: 'never' }],
-        'dot-notation': 'error',
-        eqeqeq: 'error',
-        'no-duplicate-imports': 'error',
-        'no-nested-ternary': 'error',
-        'no-useless-computed-key': 'error',
-        'no-var': 'error',
-        'prefer-const': 'error',
-        'prefer-template': 'error',
-        /* require-atomic-updates broken per https://github.com/eslint/eslint/issues/11899 */
-        'require-atomic-updates': 'off',
-        'sort-imports': 'error',
     },
 }
