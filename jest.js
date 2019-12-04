42133a5c174b45b41fc9108b62427b9680b8aade
Noah Negin-Ulster <noah.negin-ulster@tophatmonocle.com>
2019-12-04 10:29:01 -0500

    fix: don't fail jest expect when using redux test plan

--- jest.js
+++ jest.js
@@ -8,5 +8,9 @@ module.exports = {
         'jest/prefer-to-be-null': 'error',
         'jest/prefer-to-be-undefined': 'error',
         'jest/prefer-to-have-length': 'error',
+        'jest/expect-expect': [
+            'error',
+            { assertFunctionNames: ['expect', 'testSaga', 'expectSaga'] },
+        ],
     },
 }
