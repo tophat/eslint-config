d3389055f06e88f5e98ef9fd2d85449cadcd564a
Noah <noah.negin-ulster@tophatmonocle.com>
2021-06-16 16:01:30 -0400

    feat: typescript support (#190)
    **Migration Instructions**: Ensure your project has Typescript, then add all peer dependencies listed by this project. All peer dependencies should be on the latest version (this includes the latest version of eslint). In your project's eslint config, you can remove most custom import setting overrides, such as import resolvers.
    
    You will also need to add `parserOptions.project` to your root eslint config:
    
    ```
     "parserOptions": {
          "project": "tsconfig.json"
     },
    ```

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,4 @@
 module.exports = {
     ...require('./index'),
-    settings: {
-        'import/resolver': {
-            '@tophat/eslint-import-resolver-require': {},
-            node: {},
-        },
-    },
     ignorePatterns: ['/.yarn', '/.git', '/.github', '/.vscode', '/artifacts'],
 }
---
+++ base/index.js
@@ -0,0 +1,197 @@
+const { requireResolveOptional } = require('../helpers/requireUtils')
+
+// Marked as optional in peerDependenciesMeta
+const importResolverRequire = requireResolveOptional(
+    '@tophat/eslint-import-resolver-require',
+)
+
+const rules = {
+    /* Prettier Overrides */
+    'prettier/prettier': [
+        'error',
+        {
+            printWidth: 80,
+            tabWidth: 4,
+            semi: false,
+            trailingComma: 'all' /* Reduces git diff. */,
+            singleQuote: true,
+            arrowParens: 'always', // Reduces character diff when adding Typescript types.
+        },
+    ],
+
+    /* Variables */
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'no-unused-vars': 'off', // Prefer equivalent rule from @typescript-eslint */
+    '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+            /* By ignoring rest siblings, we support using `...rest` as an alternative to
+             * a 3rd party omit function, to omit keys from an object. */
+            ignoreRestSiblings: true,
+        },
+    ],
+
+    /* Strings */
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'prefer-template': 'error',
+
+    /* Naming Convention */
+    camelcase: 'off', // disabled in favour of @typescript-eslint's version
+    '@typescript-eslint/naming-convention': [
+        'error',
+        {
+            selector: ['default'],
+            format: ['camelCase'],
+            leadingUnderscore: 'allow',
+            trailingUnderscore: 'allow',
+        },
+        {
+            selector: 'variable',
+            format: ['PascalCase', 'camelCase', 'UPPER_CASE'],
+            leadingUnderscore: 'allowSingleOrDouble',
+            trailingUnderscore: 'allowSingleOrDouble',
+        },
+        {
+            selector: 'typeLike',
+            format: ['PascalCase'],
+        },
+        // Type keys can be snake_case to reflect API schemas
+        {
+            selector: ['typeProperty', 'objectLiteralProperty'],
+            format: ['camelCase', 'snake_case', 'UPPER_CASE'],
+            leadingUnderscore: 'allow',
+            trailingUnderscore: 'allow',
+        },
+        {
+            selector: ['enumMember'],
+            format: ['UPPER_CASE'],
+        },
+    ],
+
+    /* Object Properties */
+    'dot-notation': 'error',
+    'no-useless-computed-key': 'error',
+    'no-import-assign': 'warn',
+
+    /* Conditionals */
+    eqeqeq: 'error',
+    'no-nested-ternary': 'error',
+
+    /* Behaviours */
+    /*
+     * Rule: Disallow empty function blocks.
+     * Reason Disabled: Useful for try..catch{} where we don't care about failure.
+     */
+    '@typescript-eslint/no-empty-function': 'off',
+
+    /* Broken Rules */
+    /*
+     * Rule: Disallow assignments that can lead to race conditions.
+     * Reason Disabled: The rule is broken and leads to many false positives.
+     *   See: https://github.com/eslint/eslint/issues/11899
+     */
+    'require-atomic-updates': 'off',
+
+    /* Imports */
+    'no-duplicate-imports': 'error',
+    'sort-imports': [
+        'error',
+        {
+            /* Prefer import/order declaration sort due to  autofixer */
+            ignoreDeclarationSort: true,
+        },
+    ],
+    'import/default': 'error',
+    'import/export': 'error',
+    'import/named': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-absolute-path': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/no-self-import': 'error',
+    'import/no-useless-path-segments': 'error',
+    'import/no-unresolved': 'error',
+    'import/order': [
+        'error',
+        {
+            'newlines-between': 'always',
+            /* Note: Modules are assigned groups based on checks against the module path. */
+            groups: [
+                'unknown',
+                'builtin',
+                'external',
+                'internal',
+                'parent',
+                'sibling',
+                'index',
+            ],
+        },
+    ],
+
+    /* Typescript Types */
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/ban-ts-comment': 'warn', // Discourage disabling static analysis.
+    '@typescript-eslint/ban-types': 'warn', // Discourage disabling static analysis.
+}
+
+const jsIncompatibleRules = {
+    /*
+     * Rule: Require explicit return and argument types on exported functions/classes.
+     * Reason Disabled: Not compatible with JS files, recommended by the rule docs to disable.
+     */
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+    /*
+     * Rule: Disallow require statements in favour of import statements.
+     * Reason Disabled: We don't know if JS files are transpiled, so don't bother enforcing TS import syntax.
+     */
+    '@typescript-eslint/no-var-requires': 'off',
+}
+
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+    },
+    env: {
+        node: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'prettier',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    plugins: ['import', 'prettier', '@typescript-eslint'],
+    rules,
+    overrides: [
+        {
+            files: ['**/*.js'],
+            rules: jsIncompatibleRules,
+        },
+        {
+            files: ['**/*.d.ts'],
+            rules: {
+                /* In type declaration files we often model external libraries,
+                 * in which we have no control over the name. */
+                '@typescript-eslint/naming-convention': 'off',
+            },
+        },
+    ],
+    settings: {
+        'import/external-module-folders': ['node_modules', '.yarn'],
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx', '.js', '.jsx'],
+        },
+        'import/resolver': {
+            typescript: {
+                alwaysTryTypes: true,
+            },
+            node: {},
+            ...(importResolverRequire ? { [importResolverRequire]: {} } : {}),
+        },
+    },
+}
