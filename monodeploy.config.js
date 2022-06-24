b181b1f5b40f5b47ee688572a43a82a37f47b790
Noah <noah.negin-ulster@tophatmonocle.com>
2022-06-24 10:50:24 -0400

    refactor: update to yarn 3.2.1 (#4)

--- monodeploy.config.js
+++ monodeploy.config.js
@@ -1,5 +1,15 @@
 module.exports = {
     conventionalChangelogConfig: '@tophat/conventional-changelog-config',
-    persistVersions: true,
+    maxConcurrentWrites: 1,
     changelogFilename: './CHANGELOG.md',
+    autoCommit: true,
+    autoCommitMessage: 'chore: release eslint import resolver [skip ci]',
+    plugins: process.env.PREVIEW === '1' ? [] : ['@monodeploy/plugin-github'],
+    git: {
+        push: true,
+        tag: true,
+    },
+    persistVersions: true,
+    changesetIgnorePatterns: ['**/*.test.ts'],
+    access: 'public',
 }
