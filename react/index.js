1fc41180823587844301f37ad6b69d5a501afcbb
Noah Negin-Ulster <noah.negin-ulster@tophatmonocle.com>
2022-08-02 13:14:11 -0400

    feat(react): use default arguments for function components

--- react/index.js
+++ react/index.js
@@ -17,6 +17,7 @@ module.exports = {
             'error',
             {
                 forbidDefaultForRequired: true,
+                functions: 'defaultArguments',
             },
         ],
         'react/display-name': 'off',
