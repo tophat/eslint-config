e5fa0f81333d0fe6d9dea80d2310c0c7f90ba807
Daniel Basilio <dbasilio@users.noreply.github.com>
2023-01-04 11:43:43 -0500

    feat: Add react-query eslint plugin and rules (#221)
    * feat: Add react-query eslint plugin and rules
    
    * wip: no need for IIFE

--- react/index.js
+++ react/index.js
@@ -1,17 +1,21 @@
-module.exports = {
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-    },
-    plugins: ['react', 'react-hooks', 'jsx-a11y'],
-    extends: ['plugin:react/recommended'],
-    rules: {
-        'jsx-a11y/aria-props': 'error',
-        'jsx-a11y/label-has-associated-control': 'error',
-        'jsx-a11y/mouse-events-have-key-events': 'error',
-        'jsx-a11y/role-has-required-aria-props': 'error',
-        'jsx-a11y/role-supports-aria-props': 'error',
+const plugins = []
+const rules = {}
+const pluginExtends = []
+
+const doesModuleExist = (moduleName) => {
+    try {
+        require.resolve(moduleName)
+        return true
+    } catch {
+        return false
+    }
+}
+
+const reactModule = doesModuleExist('eslint-plugin-react')
+if (reactModule) {
+    plugins.push('react')
+    pluginExtends.push('plugin:react/recommended')
+    Object.assign(rules, {
         'react/default-props-match-prop-types': 'error',
         'react/require-default-props': [
             'error',
@@ -22,9 +26,48 @@ module.exports = {
         ],
         'react/display-name': 'off',
         'react/no-unsafe': ['error', { checkAliases: true }],
+    })
+}
+
+const reactHooksModule = doesModuleExist('eslint-plugin-react-hooks')
+if (reactHooksModule) {
+    plugins.push('react-hooks')
+    Object.assign(rules, {
         'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': 'error',
+    })
+}
+
+const jsxA11yModule = doesModuleExist('eslint-plugin-jsx-a11y')
+if (jsxA11yModule) {
+    plugins.push('jsx-a11y')
+    Object.assign(rules, {
+        'jsx-a11y/aria-props': 'error',
+        'jsx-a11y/label-has-associated-control': 'error',
+        'jsx-a11y/mouse-events-have-key-events': 'error',
+        'jsx-a11y/role-has-required-aria-props': 'error',
+        'jsx-a11y/role-supports-aria-props': 'error',
+    })
+}
+
+const tanstackQueryModule = doesModuleExist('@tanstack/eslint-plugin-query')
+if (tanstackQueryModule) {
+    plugins.push('@tanstack/query')
+    Object.assign(rules, {
+        '@tanstack/query/exhaustive-deps': 'error',
+        '@tanstack/query/prefer-query-object-syntax': 'warn',
+    })
+}
+
+module.exports = {
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
     },
+    plugins,
+    extends: pluginExtends,
+    rules,
     settings: {
         react: {
             version: '16.14.0',
