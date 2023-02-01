a4d50f5c7cd7db1def8b7f9b4dd81e2ac09576af
Daniel Basilio <dbasilio@users.noreply.github.com>
2023-02-01 16:27:40 -0500

    feat: Expand list of typescript rules (#231)
    * feat: Expand list of typescript rules
    
    BREAKING CHANGE: We are expanding your list of typescript rules which requires connecting your eslint config to your tsconfig. See the typescript-eslint documentation https://typescript-eslint.io/linting/typed-linting for how to do this. If your project does not use typescript and you do not want to add a tsconfig file needlessly, disable the following rules in your config: @typescript-eslint/no-confusing-void-expression, @typescript-eslint/no-for-in-array, @typescript-eslint/prefer-includes, @typescript-eslint/prefer-reduce-type-parameter, and @typescript-eslint/prefer-string-starts-ends-with.
    
    * wip: Ignore the void keyword

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
     ...require('./index'),
-    ignorePatterns: ['/.yarn', '/.git', '/.github', '/.vscode', '/artifacts'],
+    ignorePatterns: ['/.yarn', '/.git', '/.github', '/.vscode', '/artifacts', '.eslintrc.js'],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+    },
 }
