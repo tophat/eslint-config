44816f079176271ed50b2370a1b9ed52d1d4761e
noahnu <noahnu@gmail.com>
2023-04-16 16:34:55 -0400

    fix: add recommended config

--- plugins/eslint-plugin-import/src/index.ts
+++ plugins/eslint-plugin-import/src/index.ts
@@ -1,7 +1,14 @@
 import { type ESLint } from 'eslint'
 
 const config: ESLint.Plugin = {
-    configs: {},
+    configs: {
+        recommended: {
+            plugins: ['@tophat/eslint-plugin-import'],
+            rules: {
+                '@tophat/prefer-workspace-aliases': 'off',
+            },
+        },
+    },
     rules: {
         '@tophat/prefer-workspace-aliases': require('./rules/prefer-workspace-aliases'),
     },
