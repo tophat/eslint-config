b60b677551b394dce1ffeb6f301ec3c96ab095fd
noahnu <noahnu@gmail.com>
2023-04-16 14:08:03 -0400

    refactor: add prerelease workflow

--- monodeploy.config.ts
+++ monodeploy.config.ts
@@ -1,9 +1,12 @@
-import type { RecursivePartial, MonodeployConfiguration } from '@monodeploy/types'
+import type {
+    MonodeployConfiguration,
+    RecursivePartial,
+} from '@monodeploy/types'
 
 const config: RecursivePartial<MonodeployConfiguration> = {
     conventionalChangelogConfig: '@tophat/conventional-changelog-config',
     maxConcurrentWrites: 1,
-    changelogFilename: './CHANGELOG.md',
+    changelogFilename: '<packageDir>/CHANGELOG.md',
     autoCommit: true,
     autoCommitMessage: 'chore: release eslint config [skip ci]',
     plugins: process.env.PREVIEW === '1' ? [] : ['@monodeploy/plugin-github'],
