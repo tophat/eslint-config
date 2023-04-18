6f58e0677fd5500bc572ecb950d1620576477634
Noah Negin-Ulster <noah.negin-ulster@tophatmonocle.com>
2023-04-18 11:43:46 -0400

    refactor: disable lint rule for import-resolver-require

--- eslint-import-resolver-require/src/index.ts
+++ eslint-import-resolver-require/src/index.ts
@@ -11,6 +11,7 @@ const resolve = (source: string, file: string) => {
     try {
         let moduleId = require.resolve(source, { paths: [path.dirname(file)] })
         if (process.versions.pnp && moduleId.includes('__virtual__')) {
+            // eslint-disable-next-line @typescript-eslint/no-var-requires
             moduleId = require('pnpapi').resolveVirtual(moduleId)
         }
         return { found: true, path: moduleId }
